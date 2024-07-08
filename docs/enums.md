7ed294430b0e257f9ec10cca2a40a4afc27273ac
fenggwsx <fenggwsx@foxmail.com>
2024-07-08 21:00:12 +0800

    add some basic pages

---
+++ docs/enums.md
@@ -0,0 +1,44 @@
+---
+sidebar_position: 3
+---
+
+# 枚举类型
+
+本网站用到的枚举类型
+
+## 比赛状态
+
+- 未开始（蓝色或灰色）
+- 进行中（绿色）
+- 已结束（红色）
+
+## 测评状态
+
+- Submitted*
+- In Queue*
+- Running*
+- Wrong Answer***
+- Accepted**
+- Time Limit Exceeded**
+- Idleness Limit Exceeded
+- Memory Limit Exceededed**
+- Runtime Error**
+- Denial of Judgement*
+- Compilation Error
+- Partial Score
+- Rejected
+- Checker Error
+- Pretest Passed
+
+其中星号（`*`）数量定性表示常见程度，`Accepted`必须是绿色，`Wrong Answer`必须是红色，其它状态可按需配色
+
+## 用户Magic
+
+- red: 红色
+- green: 绿色
+- teal: 青色
+- blue: 蓝色
+- purple: 紫色
+- orange: 橙色
+- grey: 灰色
+- legendary: 很特殊，首字母黑色，后面红色
---
+++ docusaurus.config.ts
@@ -0,0 +1,75 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'UI设计需求文档',
+  tagline: 'EOJ-自适应编程学习平台',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ecnu', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-CN',
+    locales: ['zh-CN'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Home',
+      logo: {
+        alt: 'Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} ECNU, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
